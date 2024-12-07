c4effe82f7b81425896ceaec04b9ba130595d569
S-Skaggs <s-skaggs@outlook.com>
2024-12-07 11:45:32 -0500

    FEAT: Completed up to 7.14

---
+++ gms/gms-client/src/app/plant/plant-add/plant-add.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plant-add',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      plant-add works!
+    </p>
+  `,
+  styles: ``
+})
+export class PlantAddComponent {
+
+}
---
+++ gms/gms-client/src/app/plant/plant-list/plant-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plant-list',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      plant-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class PlantListComponent {
+
+}
